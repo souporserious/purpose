c679a023a578bdc62d193d1a80baf50a2ab9ac56
Travis Arnold <ftntravis@gmail.com>
2017-04-26 15:50:00 -0700

    just use babel for now

--- rollup.config.js
+++ rollup.config.js
@@ -26,14 +26,3 @@ export default {
   ],
   sourceMap: true,
 }
-
-// const pkg = require( './package.json' );
-//
-// export default {
-// 	entry: 'src/index.js',
-// 	moduleName: 'theAnswerToTheQuestionOfLifeTheUniverseAndEverything',
-// 	targets: [
-// 		{ dest: pkg.main, format: 'umd' },
-// 		{ dest: pkg.module, format: 'es' }
-// 	]
-// };
